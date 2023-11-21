c02aebc181b790f3ba7dd29fe43f081b14030638
HorikawaShudai <221048@st.yoshida-g.ac.jp>
2023-11-21 10:35:22 +0900

    粘着床の走行時エフェクト処理(仮)

--- ソースファイル/TeamDProject001/Particle.cpp
+++ ソースファイル/TeamDProject001/Particle.cpp
@@ -305,3 +305,50 @@ void CParticle::InitEnemyDeath(const D3DXVECTOR3& pos)
 		);
 	}
 }
+
+//===============================
+// 粘着床のエフェクト生成
+//===============================
+void Initcohesion(const D3DXVECTOR3& pos)
+{
+	// ローカル変数宣言
+	D3DXVECTOR3 EffectPos;		// エフェクトの位置
+	D3DXVECTOR3 EffectMove;		// エフェクトの移動量
+	int nLife;					// 寿命
+	D3DXVECTOR3 randRot;		// ランダムの向き
+
+	for (int nCnt = 0; nCnt < 4; nCnt++)
+	{
+		// 向きを設定する
+		randRot.x = (float)((rand() % 629 - 314) * 0.01f);
+		EffectPos.x = pos.x + sinf(randRot.x) * 20.0f;
+
+		EffectPos.y = pos.y;
+
+		// 向きを設定する
+		randRot.z = (float)((rand() % 629 - 314) * 0.01f);
+		EffectPos.z = pos.z + cosf(randRot.z) * 20.0f;
+
+		for (int nCntDust = 0; nCntDust < 4; nCntDust++)
+		{
+			EffectMove.x = sinf(randRot.x) * (rand() % 3 + 10);
+			EffectMove.y = (float)(rand() % 3 + 1);
+			EffectMove.z = cosf(randRot.z) * (rand() % 3 + 10);
+
+			// 寿命を設定する
+			nLife = rand() % 10 + 10;
+
+			// エフェクトの生成処理
+			CEffect::Create
+			(
+				EffectPos,
+				EffectMove,
+				nLife,
+				50.0f,
+				CEffect::TYPE_DUST,
+				D3DXCOLOR(0.4f, 1.0f, 0.4f, 1.0f),
+				false
+			);
+		}
+	}
+}
