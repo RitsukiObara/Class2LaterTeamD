a0a4f112152ac350a7f71af4d338eb563a5645fe
ShionSatone <221081@st.yoshida-g.ac.jp>
2023-11-22 15:16:56 +0900

    ネズミの生き返りの円のcppとh作成

---
+++ ソースファイル/TeamDProject001/resurrection_fan.h
@@ -0,0 +1,40 @@
+//===================================
+//
+// 生き返りの円の範囲ヘッダー[resurrection_fan.h]
+// Author 佐藤根詩音
+//
+//===================================
+#ifndef _RESURRECTION_FAN_H_
+#define _RESURRECTION_FAN_H_
+
+//***********************************
+// インクルードファイル
+//***********************************
+#include "object3Dfan.h"
+
+//-----------------------------------
+// クラス定義(サンプル)
+//-----------------------------------
+class CRessrectionFan : public CObject3DFan
+{
+public:			// 誰でもアクセスできる
+
+	CRessrectionFan();			// コンストラクタ
+	~CRessrectionFan();			// デストラクタ
+
+	// メンバ関数
+	HRESULT Init(void);		// 初期化処理
+	void Uninit(void);		// 終了処理
+	void Update(void);		// 更新処理
+	void Draw(void);		// 描画処理
+
+	void SetData(void/*引数*/);		// 情報の設定処理
+
+	// 静的メンバ関数
+	static CRessrectionFan* Create(void/*引数*/);		// 生成処理
+
+private:		// 自分だけアクセスできる
+
+};
+
+#endif
