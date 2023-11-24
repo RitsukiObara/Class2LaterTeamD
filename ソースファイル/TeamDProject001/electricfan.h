02a51dac51825a1b739d72ad3fc54b31d97dea66
RitsukiObara <221134@st.yoshida-g.ac.jp>
2023-11-24 12:48:43 +0900

    扇風機の関数を仮設置

--- ソースファイル/TeamDProject001/electricfan.h
+++ ソースファイル/TeamDProject001/electricfan.h
@@ -36,15 +36,11 @@ public:			// 誰でもアクセスできる
 
 	bool Collision(D3DXVECTOR3& pos, const D3DXVECTOR3& posOld, const float fWidth, const float fHeight, const float fDepth, const CObstacle::COLLTYPE type);	// 当たり判定処理
 	bool Hit(const D3DXVECTOR3& pos, const float fWidth, const float fHeight, const float fDepth, const CObstacle::COLLTYPE type);		// ヒット処理
+	bool HitCircle(const D3DXVECTOR3& pos, const float Radius, const CObstacle::COLLTYPE type);			// 円のヒット処理
 	void Action(void) override;										// ギミック起動処理
 
 private:		// 自分だけアクセスできる
 
-	// メンバ関数
-	void Drive(void);			// 走行処理
-	void Curve(void);			// カービング処理
-	void Check(void);			// 位置の確認処理
-	void RotCalc(void);			// 方向の設定処理
 };
 
 #endif
