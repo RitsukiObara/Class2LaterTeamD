5515d7e39be972adead8a78e74db57bedfd1505a
ShotaroTaniguchi <221042@st.yoshida-g.ac.jp>
2023-12-01 19:24:41 +0900

    当たり判定修正

--- ソースファイル/TeamDProject001/collision.h
+++ ソースファイル/TeamDProject001/collision.h
@@ -35,9 +35,8 @@ namespace collision
 
 	// 今後実装予定のブロックの当たり判定
 	void BlockCollision(D3DXVECTOR3& pos, const D3DXVECTOR3& posOld, const float fWidth, const float fHeight, const float fDepth);		// ブロックの当たり判定
-	void BlockRectangleCollision(CBlock& block, D3DXVECTOR3& pos, const D3DXVECTOR3& posOld,bool bToggle);	// ブロックの矩形の当たり判定
+	void BlockRectangleCollision(CBlock& block, D3DXVECTOR3& pos, const D3DXVECTOR3& posOld,int& nCollision);	// ブロックの矩形の当たり判定
 	void BlockCircleCollision(CBlock& block, D3DXVECTOR3& pos, const D3DXVECTOR3& posOld, const float fRadius, const float fHeight);	// ブロックの円形の当たり判定
-	bool BlockRectangleCollisionBool(CBlock& block, D3DXVECTOR3& pos, const D3DXVECTOR3& posOld);
 
 	// 起伏地面の当たり判定
 	bool ElevOutRangeCollision(D3DXVECTOR3* pPos, const D3DXVECTOR3& posOld, const float fWidth);			// 起伏地面の範囲外の当たり判定
