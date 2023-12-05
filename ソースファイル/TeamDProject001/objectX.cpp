3fc1c1689cda1e53874538129ba3b504684ad1b7
RitsukiObara <221134@st.yoshida-g.ac.jp>
2023-12-06 00:45:27 +0900

    ゴミ箱のモデルを入れ替え

--- ソースファイル/TeamDProject001/objectX.cpp
+++ ソースファイル/TeamDProject001/objectX.cpp
@@ -92,7 +92,7 @@ const char* CXFile::c_apModelData[CXFile::TYPE_MAX] =		// モデルの名前
 	"data\\MODEL\\pin_box.x",			// 画鋲
 	"data\\MODEL\\Senpuki_Body.x",		// 扇風機(本体)
 	"data\\MODEL\\cup.x",				// コップ
-	"data\\MODEL\\Garbage_can.x",		// ゴミ箱
+	"data\\MODEL\\GarbageBox.x",		// ゴミ箱
 
 
 	// ネズミモデル
