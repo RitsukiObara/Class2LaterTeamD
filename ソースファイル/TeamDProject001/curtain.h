7e64e8c0307bc3ba0ad86a5a1dfb8d5a9258ad37
RitsukiObara <221134@st.yoshida-g.ac.jp>
2023-12-08 12:33:06 +0900

    カーテンの基盤を作成

--- ソースファイル/TeamDProject001/curtain.h
+++ ソースファイル/TeamDProject001/curtain.h
@@ -1,1 +1,37 @@
-#pragma once
+//===================================
+//
+// カーテンヘッダー[curtain.h]
+// Author 小原立暉
+//
+//===================================
+#ifndef _CURTAIN_H_
+#define _CURTAIN_H_
+
+//***********************************
+// インクルードファイル
+//***********************************
+#include "obstacle.h"
+
+//-----------------------------------
+// クラス定義(カーテン)
+//-----------------------------------
+class CCurtain : public CObstacle
+{
+public:			// 誰でもアクセスできる
+
+	CCurtain();				// コンストラクタ
+	~CCurtain();			// デストラクタ
+
+	// メンバ関数
+	HRESULT Init(void);		// 初期化処理
+	void Uninit(void);		// 終了処理
+	void Update(void);		// 更新処理
+	void Draw(void);		// 描画処理
+
+	void SetData(const D3DXVECTOR3& pos, const D3DXVECTOR3& rot, const TYPE type);			// 情報の設定処理
+
+private:		// 自分だけアクセスできる
+
+};
+
+#endif
