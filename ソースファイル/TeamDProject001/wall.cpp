bf6dcca1acd1c210366167cc3ccb8793978b11d2
ShotaroTaniguchi <221042@st.yoshida-g.ac.jp>
2023-12-15 15:32:11 +0900

    壁優先度変更

--- ソースファイル/TeamDProject001/wall.cpp
+++ ソースファイル/TeamDProject001/wall.cpp
@@ -17,6 +17,10 @@ CWall::CWall() : CObject3D(CObject::TYPE_MAP, CObject::PRIORITY_UI)
 {
 
 }
+CWall::CWall( PRIORITY priority) : CObject3D(CObject::TYPE_MAP,priority)
+{
+
+}
 CWall::~CWall()
 {
 
@@ -45,14 +49,13 @@ void CWall::Draw(void)
 {
 	CObject3D::Draw();
 }
-CWall* CWall::Create(D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR3 size)
+CWall* CWall::Create(D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR3 size, CObject::PRIORITY pritype)
 {
 	CWall *pWall;
 
-	pWall = new CWall;
+	pWall = new CWall(pritype);
 
 	pWall->SetType(TYPE_MAP);
-
 	pWall->m_pos = pos;
 	pWall->m_rot = rot;
 	pWall->m_size = size;
